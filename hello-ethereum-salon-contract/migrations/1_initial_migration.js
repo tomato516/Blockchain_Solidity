a3282e117c3dde23596269df9d4593e9efc72ff6
tomato516 <jchoi1516@gmail.com>
2019-04-17 15:10:53 -0400

    compiled

--- hello-ethereum-salon-contract/migrations/1_initial_migration.js
+++ hello-ethereum-salon-contract/migrations/1_initial_migration.js
@@ -1,7 +1,5 @@
 const Migrations = artifacts.require("Migrations");
-const HelloEthSalon = artifacts.require("HelloEthSalon");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
-  deployer.deploy(HelloEthSalon);
 };
